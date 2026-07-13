5c18868f84189020821da0f07156ef088ea46d0f
Ishtiak-Ahmed886 <moonishtiak03@gmail.com>
2026-07-13 15:37:22 +0600

    Add appointment module

---
+++ backend/appointments/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Appointment
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "doctor",
+        "patient",
+        "appointment_date",
+        "appointment_time",
+        "status",
+    )
+
+    list_filter = (
+        "status",
+        "appointment_date",
+    )
+
+    search_fields = (
+        "doctor__user__username",
+        "patient__user__username",
+    )
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'doctors',
     'patients',
+    'appointments',
     ]
 
 MIDDLEWARE = [
